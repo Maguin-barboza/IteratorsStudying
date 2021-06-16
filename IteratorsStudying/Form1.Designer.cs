9105d91e2d36ce9afcb4861d2e08e705d2abd933
Maguin-barboza <magno_barboza@hotmail.com>
2021-06-16 09:05:02 -0300

    Add project files.

---
+++ IteratorsStudying/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace IteratorsStudying
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstMensagens = new System.Windows.Forms.ListBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LstMensagens
+            // 
+            this.LstMensagens.FormattingEnabled = true;
+            this.LstMensagens.ItemHeight = 15;
+            this.LstMensagens.Location = new System.Drawing.Point(12, 70);
+            this.LstMensagens.Name = "LstMensagens";
+            this.LstMensagens.Size = new System.Drawing.Size(182, 154);
+            this.LstMensagens.TabIndex = 0;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(12, 276);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(181, 40);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Mostrar Mensagens";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(156, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(38, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(12, 230);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(181, 40);
+            this.btnPop.TabIndex = 4;
+            this.btnPop.Text = "Desempilhar";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 319);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(44, 15);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Parado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 375);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.LstMensagens);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstMensagens;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ IteratorsStudying/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IteratorsStudying
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> stack = new Stack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnExecutar_Click(object sender, EventArgs e)
+        {
+            labelStatus.Text = "Rodando";
+
+            await Task.Run(() =>
+            {
+                foreach (string msg in stack)
+                {
+                   this.Invoke((MethodInvoker)delegate
+                   {
+                       LstMensagens.Items.Add(msg);
+                   });
+
+                    Thread.Sleep(1000);
+                }
+            });
+
+            labelStatus.Text = "Parado";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            stack.Push(textBox1.Text);
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            LstMensagens.Items.Add(stack.Pop());
+        }
+    }
+
+
+    public class Stack<T> : IEnumerable
+    {
+        private List<T> Itens = new List<T>();
+        private int Index = -1;
+
+        public void Push(T item)
+        {
+            Index++;
+            Itens.Insert(Index, item);
+        }
+
+        public T Pop()
+        {
+            T item;
+            item = Itens[Index];
+            Index--;
+
+            return item;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = Index; i >= 0 ; i--)
+            {
+                yield return Itens[i];
+            }
+        }
+    }
+}
